f99e46a2b6e66ff5454ab57b1b1fcc6fabedec62
adamp <adamp@66.158.237.35>
2020-02-28 19:08:28 -0500

    problem 2

---
+++ polakowski_problem2/src/SavingsAccount.java
@@ -0,0 +1,30 @@
+
+
+public class SavingsAccount {
+static private double annualInterestRate;
+private double savingsBalance;
+	
+public double getsavingsBalance(){
+	return this.savingsBalance;
+}
+public void setsavingsBalance(double balance) {
+	 this.savingsBalance = balance;
+}
+
+
+public double calculateMonthlyInterest() {
+	//declaring local variable to manipulate savingsBalance
+	double monthlyInterest;
+	monthlyInterest = ((this.savingsBalance * (annualInterestRate))/12);
+	return this.savingsBalance = monthlyInterest+this.savingsBalance;
+
+}
+public void balance() {
+
+	System.out.printf("$%.2f", savingsBalance);
+}
+
+public static void modifyInterestRate(double rate) {
+	annualInterestRate = rate;
+	}
+}
